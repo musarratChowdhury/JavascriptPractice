45c652a23104e10969fd6caa2d43f7532d8b479d
musarratChowdhury <muhitlopez143@gmail.com>
2023-12-17 12:56:02 +0600

    test tasks added

---
+++ test/17th Dec/sumaiya/test1.js
@@ -0,0 +1,25 @@
+//Test Data
+
+const testArr = [
+  12, 22, 244, 55, 66, 11, 345, 978, 443, 22, -444, 55, 0, 1235665, 444365,
+];
+const testArr2 = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+
+//CHALLENGE  :  write a function that will print, only the numbers that are
+//greater than 100 and divisible by 5;
+
+function divisibleByFive(testarr) {
+  //write your code here
+}
+
+//CHALLENGE : write a function that will print,
+//only the prime numbers from the given array
+
+function filterPrimeNumbers(testarr) {
+  //write your code here
+}
+
+divisibleByFive(testArr);
+filterPrimeNumbers(testArr2);
