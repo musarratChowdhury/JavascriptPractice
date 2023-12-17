88aa6f8f9befbbd0c2ea1577f8cf5281f8ef9ec0
musarratChowdhury <muhitlopez143@gmail.com>
2023-12-17 22:26:06 +0600

    hw added

---
+++ HW/soheba/18Dec/conditional.js
@@ -0,0 +1,16 @@
+//Write a program to print if a given year is a leap year or not.
+let testYear = 2019;
+
+//Grade Calculation: Write a program that takes a student's marks as input and
+//determines their grade based on certain criteria (e.g., A, B, C, D, or F).
+// Mark >= 80 => A
+// Mark < 80 && mark >= 70 => B
+// Mark < 70 && mark >= 60 => C
+// Mark < 60 && mark >= 50 => D
+// Mark < 50 => F
+
+let studentMark = 59;
+
+//Vowel or Consonant: Write a program that checks if an input character is a vowel or a consonant.
+
+let inputCharacter = "a";
