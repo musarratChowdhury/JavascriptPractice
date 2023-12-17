14c2bfce79eb91e54ebe1b3c2ad09a3b4160b2c2
musarratChowdhury <muhitlopez143@gmail.com>
2023-12-17 22:19:15 +0600

    if/else

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("HELLO WORLD");
+let x = 100;
+let y = 101;
+let z = 99;
+
+if (x > y) {
+  // x is greater than y
+  if (x > z) {
+    console.log("x is the greatest");
+  } else {
+    console.log("z is the greatest");
+  }
+} else {
+  //y is greater than x
+  if (y > z) {
+    console.log("y is the greatest");
+  } else {
+    console.log("z is the greatest");
+  }
+}
