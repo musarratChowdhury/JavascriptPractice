1ec292ac3f08fc58d0297db84265232cc86d334e
musarratChowdhury <muhitlopez143@gmail.com>
2023-12-19 20:56:02 +0600

    homework given

---
+++ HW/fts/19Dec/conditional.js
@@ -0,0 +1,27 @@
+//Write a program to print if a given year is a leap year or not.
+let testYear = 2019;
+
+//Grade Calculation: Write a program that takes a student's marks as input and
+//determines their grade based on certain criteria (e.g., A, B, C, D, or F).
+// Mark >= 80 => A
+// Mark < 80 && mark >= 70 => B
+// Mark < 70 && mark >= 60 => C
+// Mark < 60 && mark >= 50 => D
+// Mark < 50 && mark >= 40 => E
+// Mark < 40 => F
+
+let studentMark = 59;
+
+//Vowel or Consonant: Write a program that checks if an input character is a vowel or a consonant.
+
+//check if a Quadrilateral is square or rectangle or others not by given values of its 4 arms
+let A = 12;
+let B = 13;
+let C = 13;
+let D = 12;
+
+//check if a triangle is equal, bilateral, or unequal
+
+let X = 10;
+let Y = 10;
+let Z = 10;
