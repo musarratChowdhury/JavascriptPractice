534fb191382ef5d8f80aeb13889bde3745a88175
musarratChowdhury <muhitlopez143@gmail.com>
2023-12-23 16:35:52 +0600

    functions hw added

---
+++ HW/soheba/23Dec/functionHw.js
@@ -0,0 +1,29 @@
+// Simple Calculator Function:
+// Create a function that takes two numbers and an operator (+, -, *, /) as parameters and returns the result of applying the operator on the two numbers.
+
+// Temperature Converter Function:
+// Write a function that converts Celsius to Fahrenheit or Fahrenheit to Celsius based on user input and returns the converted temperature.
+
+// Greeting Function:
+// Develop a function that takes a name as an argument and returns a personalized greeting message.
+
+// Even or Odd Checker:
+// Build a function that takes an integer as input and returns whether the number is even or odd.
+
+// Largest among Three Numbers:
+// Create a function that takes three numbers as arguments and returns the largest among them.
+
+// String Reversal Function:
+// Write a function that takes a string as input and returns the reverse of that string.
+
+// Character Counter:
+// Develop a function that takes a string as an argument and returns the count of characters in that string.
+
+// Factorial Calculator:
+// Create a function that calculates the factorial of a given number.
+
+// Palindrome Checker:
+// Write a function that checks if a given word is a palindrome (reads the same backward as forward) and returns true or false accordingly.
+
+// Leap Year Checker:
+// Develop a function that determines whether a given year is a leap year or not.
