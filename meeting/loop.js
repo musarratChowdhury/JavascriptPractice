d7097bf48d4ab9daba7462ade59a498b3a468a90
musarratChowdhury <muhitlopez143@gmail.com>
2023-12-29 12:27:14 +0600

    loop

---
+++ meeting/loop.js
@@ -0,0 +1,8 @@
+const arr = ["muhit", "sadit", "soheba", "sohely", "abdullah"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i][0] == "s" && arr[i][1] == "o") {
+    console.log(arr[i]);
+  }
+}
