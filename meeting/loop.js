4fbd2004a6681a1e465cbd9b7b67d1269e1ae974
soheba <sohebaakter9@gmail.com>
2023-12-29 12:44:38 +0600

    even  and odd

--- meeting/loop.js
+++ meeting/loop.js
@@ -1,8 +1,17 @@
 const arr = ["muhit", "sadit", "soheba", "sohely", "abdullah"];
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+//const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 10];
 
 for (let i = 0; i < arr.length; i++) {
   if (arr[i][0] == "s" && arr[i][1] == "o") {
     console.log(arr[i]);
+  }
+}
+
+//take 10 numbers and  print even and odd number
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 == 0) {
+    console.log(numbers[i]);
   }
 }
