3092cf197e591fd0b1e3cc8d59b6af78d4cf0b94
musarratChowdhury <muhitlopez143@gmail.com>
2024-02-25 12:19:59 +0600

    update

--- meeting/array.js
+++ meeting/array.js
@@ -4,6 +4,50 @@ const testArr2 = [];
 
 const uniqs = [2, 3, 4, 5, 6, 7, 8, 33];
 
+const printArrNumbers = (X) => {
+  if (Array.isArray(X)) {
+    // for (let i = 0; i < X.length; i++) {
+    //   console.log(X[i]);
+    // }
+
+    X.forEach((element, i) => {
+      if (i != 0) console.log(element, i);
+    });
+  } else {
+    console.error("THIS IS NOT AN ARRAY");
+  }
+};
+
+function AddOne(arr) {
+  // arr.forEach((x, i) => {
+  //   arr[i] = x + 1;
+  // });
+  // return arr;
+  console.log("before", arr);
+
+  return arr.map((x) => (x = x + 1));
+}
+
+function addM(arr) {
+  //each element + "M"
+}
+
+function changeArr(arr) {
+  arr.push("hello");
+}
+
+function changeValue(x) {
+  x = 1000;
+  return x;
+}
+
+const addTwoNumbers = (x, y) => {
+  x = x + 1;
+  return x + y;
+};
+
+const addTwoNumbers2 = (x, y) => x + y + 1;
+
 function getUniqueNumbers(arr) {
   const uniqueArr = [];
   if (arr.length == 0) return uniqueArr;
